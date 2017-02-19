9701104d0f371435bda85082f46e2cf20272c9f2
Azimer <Azimer@users.noreply.github.com>
2017-02-19 14:55:16 -0600

    Create LICENSE (#3)
    Added GPL v3 license to the project

--- source/UIMain.h
+++ source/UIMain.h
@@ -1,3 +1,10 @@
+/*
+ *   Apollo64 - Cross-platform Nintendo 64 emulator
+ *   Copyright (C) 2017 - Azimer - http://www.apollo64.com/
+ *	
+ *   This program is distributed under the GNU General Public License v3.
+ *   See the LICENSE document supplied with this source code for details.
+ */
 #ifndef __UIMAIN_H__
 #define __UIMAIN_H__
 /* 
--- source/main.cc
+++ source/main.cc
@@ -1,3 +1,12 @@
+/*
+ *   Apollo64 - Cross-platform Nintendo 64 emulator
+ *   Copyright (C) 2017 - Azimer - http://www.apollo64.com/
+ *	
+ *   This program is distributed under the GNU General Public License v3.
+ *   See the LICENSE document supplied with this source code for details.
+ */
+
+
 #include <iostream>
 #include "uimain.h"
 /*
